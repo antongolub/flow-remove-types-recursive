ffd8f076323ea28f62999f24f33e259657169ba0
Anton Golub <mailbox@antongolub.ru>
2022-04-07 10:27:27 +0300

    feat: provide compatibility for windows

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -6,7 +6,7 @@ import fastGlob from 'fast-glob'
 export const removeFlowTypesRecursive = async (src, dst) => {
   const baseIn = resolve(process.cwd(), src)
   const baseOut = resolve(process.cwd(), dst)
-  const inputs = fastGlob.sync(`${src}/**`, { onlyFiles: true, absolute: true })
+  const inputs = fastGlob.sync(`${slash(src)}/**`, { onlyFiles: true, absolute: true })
 
   await Promise.all(inputs.map(async (input) => {
     // console.log('input=', input)
@@ -21,4 +21,16 @@ const compile = async (src, dst) => {
   const input = await fs.readFile(src, 'utf8')
   const output = flowRemoveTypes(input)
   await fs.writeFile(dst, output.toString())
+}
+
+// https://github.com/sindresorhus/slash/blob/b5cdd12272f94cfc37c01ac9c2b4e22973e258e5/index.js#L1
+const slash = (path) => {
+  const isExtendedLengthPath = /^\\\\\?\\/.test(path)
+  const hasNonAscii = /[^\u0000-\u0080]+/.test(path) // eslint-disable-line no-control-regex
+
+  if (isExtendedLengthPath || hasNonAscii) {
+    return path
+  }
+
+  return path.replace(/\\/g, "/")
 }
